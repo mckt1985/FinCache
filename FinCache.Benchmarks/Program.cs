345fd16941ac4e634d989b6749483b87c2fff240
mckt1985 <58369494+mckt1985@users.noreply.github.com>
2023-11-05 11:35:20 +0000

    Updating Benchmarks

--- FinCache.Benchmarks/Program.cs
+++ FinCache.Benchmarks/Program.cs
@@ -1,38 +1,42 @@
-﻿using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Running;
 using FinCache.Benchmarks;
 using FinCache.InMemory.Interface;
 
 
-//BenchmarkRunner.Run<FinCacheBenchmarks>();
+
+///*************************************************
+/// Runs various benchmark tests against Fincache
+/// 
+BenchmarkRunner.Run<FinCacheBenchmarks>();
 
 
 
 
+//just few initial sequence checks
+//TestFinCache();
 
+//void TestFinCache()
+//{
+//    IFinCache cache = new FinCache.InMemory.FinCache(new() { Capacity = 10 });
 
-TestFinCache();
+//    cache.AddCache(1, 1);
+//    cache.AddCache(2, 2);
+//    cache.AddCache(3, 3);
+//    cache.AddCache(4, 4);
+//    cache.AddCache(5, 5);
+//    cache.AddCache(6, 6);
+//    cache.AddCache(7, 7);
+//    cache.AddCache(8, 8);
+//    cache.AddCache(9, 9);
+//    cache.AddCache(10, 10);
+//    //shold remove 1
+//    cache.AddCache(11, 11);
 
-void TestFinCache()
-{
-    IFinCache cache = new FinCache.InMemory.FinCache(new() { Capacity = 10 });
+//    //should be null
+//    var value = cache.GetCache(1);
 
-    cache.AddCache(1, 1);
-    cache.AddCache(2, 2);
-    cache.AddCache(3, 3);
-    cache.AddCache(4, 4);
-    cache.AddCache(5, 5);
-    cache.AddCache(6, 6);
-    cache.AddCache(7, 7);
-    cache.AddCache(8, 8);
-    cache.AddCache(9, 9);
-    cache.AddCache(10, 10);
-    //shold remove 1
-    cache.AddCache(11, 11);
+//    var result = cache.GetCache(11);
+    
+//    cache.Remove(result);
 
-    var value = cache.GetCache(1);
-
-    var result = cache.GetCache(11);
-
-    cache.Remove(result);
-
-}
+//}
